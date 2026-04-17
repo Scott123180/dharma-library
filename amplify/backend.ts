26299d608336c4b8717261e70a182610adcccbf9
scott123180 <me@scotthansen.io>
2026-04-17 19:53:04 -0400

    Add Amplify Gen 2 auth with Cognito User Pool
    Adds invite-only authentication gating the entire app behind a login
    screen. Users are created manually in the Cognito console.

---
+++ amplify/backend.ts
@@ -0,0 +1,4 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+
+export const backend = defineBackend({ auth });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import App from "./App";
 import "./styles.css";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator hideSignUp={true}>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
